91a1653246f6acb681e4672c4ee7240d8533c005
yazhisun <clAMP244>
2018-03-30 09:54:49 -0400

    Initial commit

---
+++ Q2-Q6_answers.py
@@ -0,0 +1,22 @@
+
+#q2
+#Binomial Distribution. The parameter for this distribution is the total number of participants N,
+# and the probability of surviving 5 years q
+# the probability is (choose n from N) * q^n *(1-q)^(N-n)
+
+#q3
+#  weight = stat.binom.pmf(k = 400, n = 573, p = 0.5)
+
+#4
+# Estimate of mortality probability (95% credible interval): 0.0701 (0.0589, 0.0823)
+
+#q5
+# Mean survival time and 95% projection interval: 14.3298 (12.1980, 17.5037)
+
+#q6
+# Change parameters in CalibrationSetting.py
+# OBS_N = 1146
+# OBS_N_OVER_5= 800
+# Estimate of mortality probability (95% credible interval): 0.0694 (0.0603, 0.0794)
+# Mean survival time and 95% projection interval: 14.4232 (12.5224, 16.6210)
+# The length of projection interval is smaller than previous model. It is because of larger sample size.
